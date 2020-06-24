e0841f74791b6680ddd2cc5b0effa776df194a65
Riccardo Finotello <riccardo.finotello@gmail.com>
2020-06-24 21:46:13 +0200

    Add possibility to decide the starting column
    Signed-off-by: Riccardo Finotello <riccardo.finotello@gmail.com>

--- parser.py
+++ parser.py
@@ -8,7 +8,14 @@ print('This is a parser to go from the CSV format used in the analysis\n'
       'to the same format used to generate the starting dataset.\n\n'
       'You need to pass a filename to the script at the command line\n'
       'pointing to a CSV file. The parser will produce a new file with the\n'
-      'same name and the suffix "_parsed" and extension ".dat".\n\n'
+      'same name and the suffix "_parsed" and extension ".dat".\n'
+      'A second command line argument must be included with the number of\n'
+      'the starting column from which to extract files. If omitted the\n'
+      'default behaviour is to start from the first column (i.e. column 0).\n'
+      'Examples:\n\n'
+      '         python parser.py data_file.csv 1   # start from the 2nd col.\n'
+      '         python parser.py data_file.csv 0   # start from the 1st col.\n'
+      '         python parser.py data_file.csv     # start from the 1st col.\n\n'
       'You can then import the file into Mathematica using:\n\n'
       '         Import[FILENAME, "Text"];\n\n'
       'Then just copy and paste the output and remove the initial and final\n'
@@ -19,13 +26,18 @@ print('This is a parser to go from the CSV format used in the analysis\n'
      )
 
 # there must be a command line argument
-if(len(sys.argv) == 0):
+if len(sys.argv) == 0:
     print("\n\nPlease, provide a filename as command line argument.\n\n")
     sys.exit(1)
 
 # read data
 filename = sys.argv[1]
 data = pd.read_csv(filename)
+
+# the second command line argument is the starting column
+start_col = 0
+if len(sys.argv) > 2:
+    start_col = int(sys.argv[2])
 
 # divide into solutions
 solutions = pd.unique(data['solutions'])
@@ -47,7 +59,7 @@ for sol in [0,1]:
 
     # create a list containing name of the column + corresponding values
     column_list = []
-    for i in range(1,col):
+    for i in range(start_col,col):
         number_list = list(selection.iloc[:,i].values)
         number_list.insert(0, columns[i])
         column_list.append(number_list)
